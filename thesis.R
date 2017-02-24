48c71dc5f789d877aaa973bcd14ecf930671cbf6
Oliver Belmans <oliver.belmans@c02sf4wfgvc1.local>
2017-02-24 13:07:06 +0100

    first extra variables for usage analysis

--- thesis.R
+++ thesis.R
@@ -1,9 +1,47 @@
+########################################################
 # get / set workdirectory
+########################################################
 getwd()
 setwd(dir = "/Users/oliver.belmans/Data/R_workdirectory/Thesis")
 getwd()
 
+########################################################
 # load packages
+########################################################
+pkg <- c("tidyverse", "sna")
+lapply(pkg, require, character.only = TRUE)
 
-
+########################################################
 # read sample dataset
+########################################################
+sample_cdr <- read.table("sample_cdr.txt", 
+                         header = TRUE, 
+                         sep = ",", 
+                         stringsAsFactors = FALSE)
+
+
+########################################################
+# Basic Checks
+########################################################
+# Check if caller appear as callee
+table(sample_cdr$A_NUMBER %in% sample_cdr$B_NUMBER)
+
+########################################################
+# Variable manipulations
+########################################################
+## CALL_START_DT to date format
+## Create extra date/time variables like week-, daynumber, houres
+sample_cdr$CALL_START_DT_TM <- strptime(paste(sample_cdr$CALL_START_DT, sample_cdr$CALL_START_TM),"%d%b%Y %H:%M:%S")
+sample_cdr$WEEK_NR <- format(sample_cdr$CALL_START_DT_TM, "%U")
+sample_cdr$DAY_NR <- format(sample_cdr$CALL_START_DT_TM, "%w")
+sample_cdr$DAY <- format(sample_cdr$CALL_START_DT_TM, "%a")
+sample_cdr$HOUR <- as.integer(format(sample_cdr$CALL_START_DT_TM, "%H"))
+sample_cdr$PEAK_HOUR <- ifelse(sample_cdr$HOUR >= 8 & sample_cdr$HOUR <= 18, "PEAK", "NON-PEAK")
+
+# Filter outliers or non relevant items
+## based on duration
+# todo
+
+
+
+
